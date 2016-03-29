910732032b058c43dc9fbd1ddaba42739dedf833
Johan223 <jose.gonzales@ms223.org>
2016-03-29 08:35:02 -0400

    Create countdownPt2.py

---
+++ countdownPt2.py
@@ -0,0 +1,9 @@
+number = input("Countdown from what number?")
+#Asking the user what number to start to countdown
+while number >=1:
+    #as the number is counting down, and it hits 1, it will stop
+    print number
+    number = number -1
+    #This is the actual counting down of the number
+print "boom!"
+# prints out boom at the bottom

b84d9f12de56f5b8ecc196fd715aafd344557611
Johan223 <jose.gonzales@ms223.org>
2016-04-12 08:15:00 -0400

    Update April11_DoNow.py

--- April11_DoNow.py
+++ April11_DoNow.py
@@ -9,4 +9,6 @@ def mystery_function(x, y):
     result z
 mystery_function(1, 2)
 #When we run this code the return function doesn't print out the result, 
-#but when the print code is put in it does print out the result, or the random number. You could only only know the result if you put in the print code. 
+#but when the print code is put in it does print out the result, or the random number. You could only only know the result if you 
+#put in the print code. 
+#Also, the return function s not known to the person but te result can be use for the computer in other functions.

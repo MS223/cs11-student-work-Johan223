200cc48724e747f56cd20e045a8ae67e3426d452
Johan223 <jose.gonzales@ms223.org>
2016-04-19 08:53:26 -0400

    Update War!.py

--- War!.py
+++ War!.py
@@ -10,6 +10,9 @@ deck = shuffled_deck()
 player_name = raw_input("What is your name, player 1?")
 player_name2=raw_input("Whats your name, player 2?")
 
+play_name_score = 0
+play_name_score2 = 0
+
 def player_turn(player_name):
 	card= deck.pop()
 	print player_name,'drew card', card
@@ -17,8 +20,11 @@ def player_turn(player_name):
 player_turn(player_name)
 player_turn(player_name2)
 
-if player_name > player_name2:
-	player_name_score + 1
-elif player_name2 > player_name:
-	player_name2_score + 1
+while deck:
+	if player_turn(player_name) > player_turn(player_name2):
+		player_name_score = player_name_score = 1
+	elif player_turn(player_name2)> player_turn(player_name):
+		player_name_score2 = play_name_score2
+
+
 	#This code needs more cards to be drawn and then added to see who wins in results

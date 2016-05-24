df3572a31e5f4e2b1ec53e5fc8f2ef19d113a898
Johan223 <jose.gonzales@ms223.org>
2016-05-24 09:29:14 -0400

    Create Colors.py

---
+++ Colors.py
@@ -0,0 +1,13 @@
+class color(object): 
+    Navy=color()
+    Cyan=color()
+    Purple=color()
+        
+    Navy.r= 0
+    Navy.g=0
+    Navy.b=128
+    
+    Cyan.r=
+    Cyan.g=
+    Cyan.b=
+def add_color(color1,color2):
